71a7975439ffcf883aaa322b13e8b2ab325f9377
Samay Maheshwari <148616279+SamayMaheshwari@users.noreply.github.com>
2025-03-25 01:29:42 +0530

    Add files via upload

---
+++ generate_data.py
@@ -0,0 +1,16 @@
+import mysql.connector
+import time
+import random
+
+db = mysql.connector.connect(host="localhost", user="root", passwd="", database="process_monitoring")
+cursor = db.cursor()
+
+while True:
+    cursor.execute("DELETE FROM processes")
+    for i in range(10):
+        name = f"Process_{i}"
+        cpu = random.randint(1, 100)
+        memory = random.randint(1, 100)
+        cursor.execute("INSERT INTO processes (id, name, cpu, memory) VALUES (%s, %s, %s, %s)", (i, name, cpu, memory))
+    db.commit()
+    time.sleep(3)
